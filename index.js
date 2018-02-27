92d1eab133bb23cfe79a8bdcc922046f9f5532b9
asimov <briangroat93@gmail.com>
2018-02-27 17:38:34 -0500

    Updating display logic

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ appointmentDataRef.once("value")
       document.getElementById("appointment_status").innerHTML = appointment_status;
   });
 });
+
+  $("#book").click(() => {
+
+    var currentStatus = $("#appointment_status").text();
+    var email = $("#email").val();
+
+    if(email  === ""){
+      alert('Please insert email address')
+      return false;
+    }
+    if( currentStatus == "PENDING" ||  currentStatus =="CLOSED" || currentStatus == "") {
+      alert('This appointment is not currently available');
+      return false;
+    }
+    db.ref("Appointments").update({ appointment: "PENDING", email:email });
+    alert('Thank you for your interest. Your appointment request is currently pending')
+  })
